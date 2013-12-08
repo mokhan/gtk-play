5d6eacb26dd2a2844fb7d3c4ddedb2c809498b58
mo khan <mo@mokhan.ca>
2013-12-07 18:58:07 -0700

    add hard coded menu.

--- lib/presentation/presenters/application_shell_presenter.rb
+++ lib/presentation/presenters/application_shell_presenter.rb
@@ -10,8 +10,32 @@ class ApplicationShellPresenter
       puts "publishing halt to event aggregator"
       @event_aggregator.publish(:halt)
     end
+    add_menu_to(@view)
     @view.set_default_size 250, 200
     @view.set_window_position Gtk::Window::POS_CENTER
     @view.show_all
+  end
+
+  private
+
+  def add_menu_to(view)
+    view.modify_bg(Gtk::STATE_NORMAL, Gdk::Color.new(6400, 6400, 6440))
+    menu_bar = Gtk::MenuBar.new
+
+    file_menu = Gtk::Menu.new
+    file_menu_item = Gtk::MenuItem.new("_File")
+    file_menu_item.set_submenu(file_menu)
+
+    exit_menu_item = Gtk::MenuItem.new("E_xit")
+    exit_menu_item.signal_connect "activate" do
+      @event_aggregator.publish(:halt)
+    end
+
+    file_menu.append(exit_menu_item)
+    menu_bar.append(file_menu_item)
+
+    vbox = Gtk::VBox.new(false, 2)
+    vbox.pack_start(menu_bar, false, false, 0)
+    view.add(vbox)
   end
 end
