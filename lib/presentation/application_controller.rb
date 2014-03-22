3b764cbf6f6665a5092293094ae202756a997ff0
mo khan <mo@mokhan.ca>
2014-03-21 22:49:21 -0600

    remove commented out and unused code.

--- lib/presentation/application_controller.rb
+++ lib/presentation/application_controller.rb
@@ -4,20 +4,6 @@ class ApplicationController
   end
 
   def run(presenter_key)
-    #presenter = @container.resolve(presenter_key)
-    #view = presenter.view
-    #view.builder = create_builder_for(presenter_key.to_s)
-    #view.bind_to(presenter)
     @presenter_factory.create(presenter_key).present
-  end
-
-  private
-
-  def create_builder_for(presenter_key)
-    builder = Gtk::Builder.new
-    file = File.join(File.dirname(__FILE__), 'windows', "#{presenter_key.gsub(/\_presenter/, '')}.xml")
-    puts "LOADING #{presenter_key} FROM #{file}"
-    builder.add_from_file(file)
-    builder
   end
 end
