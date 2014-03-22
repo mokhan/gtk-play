5c9e7aebb75ffb4e75ab4705b6c61e63c71453a7
mo khan <mo@mokhan.ca>
2014-03-21 21:52:19 -0600

    extract constant for target API URL.

--- lib/service/stock_service.rb
+++ lib/service/stock_service.rb
@@ -1,12 +1,11 @@
 class StockService
+  API_URL = 'http://download.finance.yahoo.com/d/quotes.csv'
   def initialize(http_gateway = HttpGateway.new)
     @http_gateway = http_gateway
   end
 
-  #http://download.finance.yahoo.com/d/quotes.csv?s=%40%5EDJI,GOOG&f=nsl1op&e=.csv
   def fetch(symbol)
-    #map_from(@http_gateway.get('http://download.finance.yahoo.com/d/quotes.csv', {s: 'GOOG', f: 'nsl1', e: '.csv'}))
-    map_from(@http_gateway.get('http://download.finance.yahoo.com/d/quotes.csv', {s: symbol, f: 'nsl1', e: '.csv'}))
+    map_from(@http_gateway.get(API_URL, {s: symbol, f: 'nsl1', e: '.csv'}))
   end
 
   private
